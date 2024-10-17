ebdb6e2c5634712d6a07aa6b05be313e6bb84047
Artem Basalaev <basa62.1996@gmail.com>
2024-10-17 02:09:40 +0200

    variables

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello Go World!")
+	shostGolang := "Watch Go crash course"
+	fullGolang := "Watch Nana's Golang Full Course"
+	rewardDessert := "Reward myself with rewardDessert donut"
+
+	fmt.Println("##### Welcome to our Todolist App! #####")
+
+	fmt.Println("List of my Todos")
+	fmt.Println(shostGolang)
+	fmt.Println(fullGolang)
+	fmt.Println(rewardDessert)
 }
